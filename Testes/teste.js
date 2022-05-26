dde978055d8d4639b89ffa04c26d04f56b8b38b0
WL777777 <wallace.lins19@gmail.com>
2022-05-25 22:20:03 -0300

    Aula 10

---
+++ Testes/teste.js
@@ -0,0 +1,12 @@
+
+ checkbox.addEventListener('click', (event)  => {
+    if (checkbox.classList.contains('on')) {
+        checkbox.setAttribute('aria-checked', 'false');
+    } else {
+        checkbox.setAttribute('aria-checked', 'true');
+    }
+
+    checkbox.classList.toggle('on');
+});
+
+
